7c000ccb7535d3cc342ea01d4922bdffd1b77177
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-01-02 23:37:28 +0530

    created new db for temparory users

---
+++ db.js
@@ -0,0 +1,28 @@
+export const users = [
+    {
+        "id": "23131",
+        "firstName": "Utkarsh",
+        "lastName": "Sharma",
+        "email": "megayt750@gmail.com",
+        "password": "12345"
+    },
+    {
+        "id": "4232",
+        "firstName": "John",
+        "lastName": "Doe",
+        "email": "dohjohn@gmail.com",
+        "password": "1234545"
+    },
+]
+
+
+export const quotes = [
+    {
+        name: "I turn coffee into code",
+        by: "23131"
+    },
+    {
+        name: "if it works don't touch it",
+        by: "4232"
+    }
+]
--- server.js
+++ server.js
@@ -1,26 +1,43 @@
-import { ApolloServer , gql } from "apollo-server"
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
-
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { users, quotes } from './db.js'
 const typeDefs = gql`
 type Query{
-    greet: String
+    users: [User]
+    user(id:ID!): User
+    quotes: [Quote]
 }
-`
+type User {
+    id: ID!
+    firstName: String
+    lastName: String
+    email: String
+    quotes: [Quote]
+}
+
+type Quote {
+    name: String
+    by: ID!
+}
+`;
 
 const resolvers = {
-    Query:{
-        greet:() => "Hello World"
-    }
-}
+  Query: {
+    users:() => users,
+    user: (_, {id}) => users.find(user => user.id == id),
+    quotes: () => quotes
+  },
+  User: {
+    quotes: (ur) => quotes.filter(quote => quote.by == ur.id)
+  }
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins:[
-        ApolloServerPluginLandingPageGraphQLPlayground()
-    ]
-})
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
 
 server.listen().then(({ url }) => {
-    console.log(`Server ready at ${url}`);
-})
+  console.log(`Server ready at ${url}`);
+});

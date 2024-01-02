bb8335acf5e0e6a265d608b9acf7738dc7106e21
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-01-03 00:47:53 +0530

    updated new files and features

--- server.js
+++ server.js
@@ -1,38 +1,9 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-import { users, quotes } from './db.js'
-const typeDefs = gql`
-type Query{
-    users: [User]
-    user(id:ID!): User
-    quotes: [Quote]
-    iquote(by:ID): [Quote]
-}
-type User {
-    id: ID!
-    firstName: String
-    lastName: String
-    email: String
-    quotes: [Quote]
-}
+import { typeDefs } from "./schemaGql.js";
+import resolvers from "./resolvers.js";
 
-type Quote {
-    name: String
-    by: ID!
-}
-`;
 
-const resolvers = {
-  Query: {
-    users:() => users,
-    user: (_, {id}) => users.find(user => user.id == id),
-    quotes: () => quotes,
-    iquote: (_, {by}) => quotes.filter(quote => quote.by == by)
-  },
-  User: {
-    quotes: (ur) => quotes.filter(quote => quote.by == ur.id)
-  }
-};
 
 const server = new ApolloServer({
   typeDefs,
